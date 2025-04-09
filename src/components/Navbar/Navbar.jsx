2c4c8283338289e761866fd456a0303dd53902ea
Saurabh Shelar <saurabhshelar512@gmail.com>
2025-04-09 13:04:11 +0530

    made navbar responsive

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,15 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './navbar.css'
-import {Link} from 'react-router-dom'
-import { FaUser, FaShoppingCart } from "react-icons/fa";
+import { Link } from 'react-router-dom'
+import { FaUser, FaShoppingCart, FaTimes, FaBars } from "react-icons/fa";
 import Logo from '../../assets/logo.png';
+
 const Navbar = () => {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+
+  const handleCloseLi = () => {
+    setSidebarOpen(false)
+  }
+
+
   return (
     <>
-      <nav>
-      <h1>
-      <img src={Logo} alt="logo"/></h1>
-        <ul>
+      <nav className='desktop-nav'>
+        <h1>
+          <img src={Logo} alt="logo" /></h1>
+
+        <div onClick={() => setSidebarOpen(!sidebarOpen)} className='Hamburger-icons'>
+          {sidebarOpen ? <FaTimes size={30} /> : <FaBars size={30} />}
+        </div>
+
+        <ul className='desktop-ul'>
           <li><Link to={'/'}>Home</Link></li>
           <li><Link to={'/products'}>Products</Link></li>
           <li><Link to={'/about'}>About us</Link></li>
@@ -17,10 +30,26 @@ const Navbar = () => {
         </ul>
 
         <span className='cart-icons'>
-          <FaUser id='user' size={"25px"}/>
-          <FaShoppingCart id='cart' size={"25px"}/>
+          <FaUser id='user' size={"25px"} />
+          <FaShoppingCart id='cart' size={"25px"} />
         </span>
       </nav>
+
+        <nav  className={`mobile-nav ${sidebarOpen ? "display-block" : "display-none"}`}>
+
+        <ul className='mobile-ul'>
+          <li><Link onClick={handleCloseLi} to={'/'}>Home</Link></li>
+          <li><Link onClick={handleCloseLi} to={'/products'}>Products</Link></li>
+          <li><Link onClick={handleCloseLi} to={'/about'}>About us</Link></li>
+          <li><Link onClick={handleCloseLi} to={'/contact'}>Contact</Link></li>
+        <span className='cart-icons-mobile'>
+          <FaUser id='user' size={"25px"} />
+          <FaShoppingCart id='cart' size={"25px"} />
+        </span>
+        </ul>
+        </nav>
+
+
     </>
   )
 }
